8e165f4fbdf4d2b2ccb52b83fd6fbb562085a2ee
rokotsu <garretdejl@gmail.com>
2024-01-14 01:55:02 +0500

    adding explicit relationships in ORM

--- app/articles/models.py
+++ app/articles/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Date, ForeignKey, Integer, String
+from sqlalchemy import Column, Date, ForeignKey, String
 from app.database import Base
+from sqlalchemy.orm import relationship
 
 class Articles(Base):
     __tablename__ = "articles"
@@ -8,3 +9,5 @@ class Articles(Base):
     content = Column(String, nullable=False)
     date_publication = Column(Date, nullable=False)
     author = Column(ForeignKey("users.username"))
+
+    author_username = relationship("Users", back_populates="articles")
--- app/users/models.py
+++ app/users/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -10,3 +11,6 @@ class Users(Base):
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
     role = Column(String, nullable=False)
+
+    articles = relationship("Articles", back_populates="author_username")
+
