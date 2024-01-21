2eca767e5d2c890a1996ec0218b13ad798f2071f
rokotsu <garretdejl@gmail.com>
2024-01-21 22:14:48 +0500

    prepare_tests

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
     LOG_LEVEL: str
 
     DB_HOST: str
@@ -19,6 +22,14 @@ class Settings(BaseSettings):
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+    @property
+    def TEST_DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
     class Config:
         env_file = ".env"
 
