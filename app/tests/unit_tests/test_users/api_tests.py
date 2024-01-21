83c140ad802eb17cb0a201e7d58a864de3efb4a6
rokotsu <garretdejl@gmail.com>
2024-01-22 03:46:48 +0500

    401

--- app/tests/unit_tests/test_users/api_tests.py
+++ app/tests/unit_tests/test_users/api_tests.py
@@ -18,16 +18,16 @@ async def test_register_user(username, email, password, role, status_code, ac: A
     assert response.status_code == status_code
 
 
-@pytest.mark.parametrize("username,email,password,role,status_code",[
-    ("roma", "artem@email.com", "artem", "admin", 200),
-    ("Rebeca", "emaile@email.com", "test", "user", 200)
+@pytest.mark.parametrize("username,password,status_code",[
+    ("roma", "artem", 200),
+    ("Rebeca", "test", 200),
+    ("Rebecaa", "test", 401),
+    ("Rebeca", "tsest", 401)
 ])
-async def test_login_user(username, email, password, role, status_code, ac: AsyncClient):
+async def test_login_user(username, password, status_code, ac: AsyncClient):
     response = await ac.post("/auth/login", json={
         "username": username,
-        "email": email,
         "password": password,
-        "role": role
     })
     print(response)
     assert response.status_code == status_code
